80c6c8f07b4797d6a915cd11f9ba022631f29d98
Chaikovsky Alexandr <alexchay@gmail.com>
2023-05-09 19:44:14 +0200

    chore(vscode): add configuration files

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ import os
 import pprint
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     #
     pp = pprint.PrettyPrinter(indent=2)
     pp.pprint(sys.path)
     #
-    print(os.environ.get('ENVIRONMENT'))
+    print(os.environ.get("ENVIRONMENT"))
